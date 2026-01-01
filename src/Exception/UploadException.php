0d9f74462d687316abf85302cb9ba6742a153126
Fordi Malanda <fordimalanda7@gmail.com>
2026-01-01 16:31:04 +0100

    feat: Ajout d'une exception personnalisée pour le processus d'upload

---
+++ src/Exception/UploadException.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SfUpload\Exception;
+
+use RuntimeException;
+
+class UploadException extends RuntimeException
+{
+    // Classe simple pour identifier nos erreurs spécifiques
+}
